84edf289fc3975fa5c8daa3e48d008cbb671a287
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-10-04 20:04:07 +0200

    update

--- app.js
+++ app.js
@@ -57,21 +57,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // payment option
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const oneTimeRadio = document.getElementById('one-time');
     const monthlyRadio = document.getElementById('monthly');
     const priceElement = document.getElementById('price');
     const totalPriceElement = document.querySelector('.total-price');
-  
-    oneTimeRadio.addEventListener('change', function() {
-        priceElement.textContent = '0 DKK'; // Set one-time payment price
-        totalPriceElement.textContent = '198 DKK'; // Update total price
-    });
-  
-    monthlyRadio.addEventListener('change', function() {
 
-      priceElement.textContent = '20%'; // Set monthly payment price
-      totalPriceElement.textContent = '160 DKK'; // Update total price
+    oneTimeRadio.addEventListener('change', function () {
+        priceElement.textContent = '0 DKK'; // Set one-time payment price
+        totalPriceElement.textContent = '200 DKK'; // Update total price
     });
-  });
-  
+
+    monthlyRadio.addEventListener('change', function () {
+
+        priceElement.textContent = '20%'; // Set monthly payment price
+        totalPriceElement.textContent = '160 DKK'; // Update total price
+    });
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     document.getElementById('signupForm').addEventListener('submit', function (e) {
+//         e.preventDefault(); // Prevents the default form submission
+
+//         // Show popup message
+//         var popup = confirm('Your form has been submitted successfully!');
+//         if (popup) {
+//             window.location.href = 'index.html'; // Redirect to a new page on OK
+//         }
+//     });
+// });
+
+
