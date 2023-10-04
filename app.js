53a5c1bea75777ab50ab008bd77a41678455a45e
MajedDadou <91125807+MajedDadou@users.noreply.github.com>
2023-10-04 15:16:33 +0200

    faktura

--- app.js
+++ app.js
@@ -55,3 +55,23 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+// payment option
+document.addEventListener('DOMContentLoaded', function() {
+    const oneTimeRadio = document.getElementById('one-time');
+    const monthlyRadio = document.getElementById('monthly');
+    const priceElement = document.getElementById('price');
+    const totalPriceElement = document.querySelector('.total-price');
+  
+    oneTimeRadio.addEventListener('change', function() {
+        priceElement.textContent = '0 DKK'; // Set one-time payment price
+        totalPriceElement.textContent = '198 DKK'; // Update total price
+    });
+  
+    monthlyRadio.addEventListener('change', function() {
+
+      priceElement.textContent = '20%'; // Set monthly payment price
+      totalPriceElement.textContent = '160 DKK'; // Update total price
+    });
+  });
+  
